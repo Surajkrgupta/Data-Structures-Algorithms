c8738b886d92dac12e8f87013bdf151f21b0833c
Suraj Kumar Gupta <124661478+Surajkrgupta@users.noreply.github.com>
2026-06-14 10:02:24 +0530

    Time: 5 ms (58.50%) | Memory: 101.2 MB (48.21%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -10,27 +10,30 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        ListNode f=head,s=head;
-        while(f!=null && f.next!=null){
-            f=f.next.next;
-            s=s.next;
+        int res = Integer.MIN_VALUE;
+
+        ListNode s = head, f = head;
+        while (f != null && f.next != null) {
+            f = f.next.next;
+            s = s.next;
         }
-        f=head;
-        ListNode p=null;
-        while(f!=s){
-            ListNode n=f.next;
-            f.next=p;
-            p=f;
-            f=n;
+        ListNode p = null;
+        ListNode c = s;
+        while (c != null) {
+            ListNode n = c.next;
+            c.next = p;
+            p = c;
+            c=n;
         }
-        head=p;
-        int m=Integer.MIN_VALUE;
-        while(s!=null){
-            int sum=head.val+s.val;
-            m=Math.max(sum,m);
-            head=head.next;
-            s=s.next;
+        ListNode h1 = head;
+        ListNode h2 = p;
+        while (h2 != null) {
+            int sum = h1.val + h2.val;
+            res = Math.max(res, sum);
+            h1 = h1.next;
+            h2 = h2.next;
         }
-        return m;
+        return res;
+
     }
 }
