a8e57411d1a7cba2dfce8f65a31dd0b4a277b1bc
Suraj Kumar Gupta <124661478+Surajkrgupta@users.noreply.github.com>
2026-06-22 08:54:04 +0530

    Time: 4 ms (63.02%) | Memory: 43 MB (96.50%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int b = 0;
+        int a = 0;
+        int l = 0;
+        int o = 0;
+        int n = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (ch == 'b')
+                b++;
+            else if (ch == 'a')
+                a++;
+            else if (ch == 'l')
+                l++;
+            else if (ch == 'o')
+                o++;
+            else if (ch == 'n')
+                n++;
+        }
+        int[] values = { b, a, l / 2, o / 2, n };
+
+        int min = values[0];
+        for (int v : values) {
+            min = Math.min(min, v);
+        }
+
+        return min;
+    }
+}
