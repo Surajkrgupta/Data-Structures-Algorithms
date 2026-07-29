9b8999a90e420d0f78cafc7474cab02797a79038
Suraj Kumar Gupta <124661478+Surajkrgupta@users.noreply.github.com>
2026-07-29 23:10:15 +0530

    Time: 2 ms (70.80%) | Memory: 56 MB (6.69%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ele=nums[0];
+        int c=1;
+        for(int i=1;i<nums.length;i++){
+            if(ele==nums[i]) c++;
+            else if(ele!=nums[i]) c--;
+            if(c==0){
+                ele=nums[i];
+                c=1;
+            }
+        }
+        return ele;
+    }
+}
